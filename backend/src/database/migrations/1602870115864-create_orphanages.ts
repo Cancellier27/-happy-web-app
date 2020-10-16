31508cb28f0ed53a485cb02dcfaf50903865cccc
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-10-16 18:52:38 +0100

    creating first table, method up

---
+++ backend/src/database/migrations/1602870115864-create_orphanages.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createOrphanages1602870115864 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // realizar as alterações desejadas no banco de dados
+    // criar tabela, novo campo, deletar campo...
+
+    await queryRunner.createTable(new Table({
+      name: 'orphanages',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'latitude',
+          type: 'decimal',
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: 'longitude',
+          type: 'decimal',
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: 'aboute',
+          type: 'text',
+        },
+        {
+          name: 'instructions',
+          type: 'text',
+        },
+        {
+          name: 'open_on_weekends',
+          type: 'boolean',
+          default: false,
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Desfazer o que foi feito em UP
+  }
+
+}
