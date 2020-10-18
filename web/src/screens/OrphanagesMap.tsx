8ffd05d6dcb00603e81785994ac97c670b603660
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-10-18 23:06:15 +0100

    add 2 pages; 2 pages added directly from the course

--- web/src/screens/OrphanagesMap.tsx
+++ web/src/screens/OrphanagesMap.tsx
@@ -48,15 +48,15 @@ function OrphanagesMap() {
         >
           <Popup closeButton={false} minWidth={240} maxWidth={240} className='map-popup' >
             Lar das menins
-            <a href="#">
+            <Link to="/orphanages/1">
               <FiArrowRight size={20} color="fff" />
-            </a>
+            </Link>
           </Popup>
         </Marker>
         
       </Map>
 
-      <Link to="" className="create-orphanage" >
+      <Link to="/orphanages/create" className="create-orphanage" >
           <FiPlus size={32} color="#fff" />
       </Link>
     </div>
