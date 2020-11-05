8a964feb15cfcb3d50040ef2fb36da0ec7ddc80b
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-11-05 20:47:06 +0000

    creating css fot marker comment

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons'
 
 import mapMarker from "./src/images/map-marker.png"
 
@@ -20,12 +21,30 @@ export default function App() {
 
         <Marker
           icon={mapMarker}
+          calloutAnchor={{
+            x: 2.8,
+            y: 0.8,
+          }}
           coordinate={{
             latitude: -27.2092052,
             longitude: -49.6401092
           }}
-        />
+        >
+          <Callout tooltip onPress={() => { alert('oi') }} >
+            <View style={styles.calloutContainer} >
+              <Text style={styles.calloutText}> Mundo Encantado </Text>
+            </View>
+          </Callout>
+        </Marker>
       </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}> 2 orphanages encontrados </Text>
+
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { alert('oi') }} >
+          <Feather name='plus' size={20} color='#fff' />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -38,5 +57,33 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 168,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 16,
+    justifyContent: "center",
+
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+  },
+
+  footer: {
+
+  },
+
+  footerText: {
+
+  },
+
+  createOrphanageButton: {
+
   }
+
 });
